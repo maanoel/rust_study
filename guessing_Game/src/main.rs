d02281fb3f6e785f151fb853657771485e8d0c00
Manoel Vitor Brito Moura <manoel.vitor@bh01.local>
2020-06-26 02:12:38 -0300

    rust studying

--- guessing_Game/src/main.rs
+++ guessing_Game/src/main.rs
@@ -4,6 +4,11 @@ use std::io;
 
 fn main(){
 
+	let _f: bool = false;
+	
+	my_tuple();
+	my_array();
+
 	loop{
         println!("guess the number");
 		
@@ -23,13 +28,47 @@ fn main(){
 			Ordering::Less => println!("Too less"),
 			Ordering::Greater => println!("Too big"),
 			Ordering::Equal => {
-			    println!("you win!")
+			    println!("you win!");
 				break;
 			}
 		}
 		
+		if guess > 100 {
+			println!("Its to high");
+		}
+		
 		println!("the secret is {}", _screte_number);
+		
 	
 	}
 	
+	fn my_tuple(){
+		
+		let  _tup = (1, 2, 3);
+		let (_x, _y, _z ) = _tup;
+		
+		println!("the full tuple is: {} {} {}", _tup.0, _tup.1, _tup.2);
+		println!("the vale is {}", _x);
+	}
+	
+	fn my_array(){
+		let _months = ["January", "February"];
+		
+		println!("the array index {}", _months[0]);
+		
+		crazy_function();
+	}
+	
+	fn crazy_function() -> i32{
+	    5
+	}
+	
+	fn ternary(){
+	    let condition = true;
+		let value =  if condition {5} else {4};
+	}
+	
+	fn teste_string(){
+		let _value = String::from("hi");
+	}
 }
